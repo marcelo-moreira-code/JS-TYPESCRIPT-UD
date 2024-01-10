2ea8bdd8df7c1433995880587ab9e71033d448d0
Marcelo S. Moreira <120063058+marcelo-moreira-code@users.noreply.github.com>
2024-01-10 11:46:29 -0400

    Create comentarios.js

---
+++ JSS/aula04/comentarios.js
@@ -0,0 +1,12 @@
+// Este é um comentários em linha
+
+/*
+    Este é um comentário em várias linhas 
+    Este é um comentário em várias linhas 
+    Este é um comentário em várias linhas 
+    Este é um comentário em várias linhas 
+*/
+console.log("Olá mundo!");
+console.log("Linha 1");
+console.log("linha 2"); // console.log("Não exibido")
+console.log("Exibida")
