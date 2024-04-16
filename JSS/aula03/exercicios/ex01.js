be1574ec78e22635c7b30905bebb168a942ec0d6
Marcelo S. Moreira <120063058+marcelo-moreira-code@users.noreply.github.com>
2024-04-16 10:39:00 -0400

    c

--- JSS/aula03/exercicios/ex01.js
+++ JSS/aula03/exercicios/ex01.js
@@ -1,2 +1,4 @@
 console.log("Meu nome é 'Marusero'. Estou aprendendo JS às", 10 ,"da manhã.")
 console.log("Luana tem ",17, "anos e gosta de ler webnovels por volta dás", 7, "da noite.")
+
+console.log("Meu nome é 'Marcelo'. Estou aprendendo JS às",10 ," horas e", 34,"min", "da manhã." )
--- JSS/aula03/index.js
+++ JSS/aula03/index.js
@@ -1,3 +1,5 @@
 console.log("Japanese");
 console.log('Luiz P');
 console.log("Ana disse: 'Saia já daqui!'")
+console.log("Ana tem '19' anos ")
+console.log("Bom dia , Karen!")
