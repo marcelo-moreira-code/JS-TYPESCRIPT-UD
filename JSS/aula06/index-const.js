4690f16f402d1a8a259f86304a9d45b8709df9b9
Marcelo S. Moreira <120063058+marcelo-moreira-code@users.noreply.github.com>
2024-04-17 11:39:26 -0400

    Create index-const.js

---
+++ JSS/aula06/index-const.js
@@ -0,0 +1,34 @@
+// Não se redeclara constantes como let
+
+/* 
+ // Não podemos criar constantes com palavras reservadas
+ // constantes pecisam ter nomes significativos
+ // Não pode começar com números
+ // Não podem conter espaços ou traços
+ // Utilizammos camelCase
+ 
+ // Não podemos mudar o valor de uma constante
+
+
+ let nomeCliente = "Luana";
+ let idadeCliente = " ";
+ 
+const pi = 3.14;
+
+*/
+ 
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const soma = primeiroNumero + segundoNumero;
+const multiplicacao = primeiroNumero * segundoNumero;
+let nomeDiretor = "Jerônimo";
+let resultadoTriplicado = multiplicacao * 3; 
+
+console.log(soma);
+console.log(multiplicacao);
+
+console.log(resultadoTriplicado);
+
+console.log(typeof(nomeDiretor));
+
+console.log(typeof(resultadoTriplicado));
