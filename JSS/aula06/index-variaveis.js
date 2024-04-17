efa157acbe3d6441a07b85cb2985f48cd8a32a8a
Marcelo S. Moreira <120063058+marcelo-moreira-code@users.noreply.github.com>
2024-04-17 10:38:04 -0400

    v

---
+++ JSS/aula06/index-variaveis.js
@@ -0,0 +1,31 @@
+ let nome; // Declarando a variável
+ nome = "Lucas"; // Inicializando a var
+ // Não se redeclara variáveis variáveis como let
+
+/* 
+ // Não podemos criar variáveis com palavras reservadas
+ // Variáveis pecisam ter nomes significativos
+ // Não pode começar com números
+ // Não podem conter espaços ou traços
+ // Utilizammos camelCase
+ 
+ // Utilize preferencialmente let e não var.
+
+
+ let nomeCliente = "Luana";
+ let idadeCliente = " ";
+
+
+ */
+
+
+
+/*
+let nome = "João";
+
+console.log(nome,"nasceu em 1984.")
+console.log("Em 2000,",nome, "conheceu Maria.")
+console.log(nome,"casou-se com Maria em 2010.")
+console.log("Maria teve 1 filha com",nome," em 2012.")
+console.log("A filha de",nome, "e Maria se chama Helena.")
+*/
