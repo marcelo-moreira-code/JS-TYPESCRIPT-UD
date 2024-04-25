bad622cfe0175ed05e9a49cccc1b6d89ca2c10d4
Marcelo S. Moreira <120063058+marcelo-moreira-code@users.noreply.github.com>
2024-04-25 12:11:30 -0400

    Update ex.js

--- JSS/aula13/ex/ex.js
+++ JSS/aula13/ex/ex.js
@@ -1,6 +1,9 @@
 let nome = prompt("Digite seu nome completo:");
 
-window.document.body.innerHTML += "Alguma coisa qualquer. <br />";
+document.body.innerHTML += `Seu nome é: ${nome}  <br />`;
+document.body.innerHTML += `Seu nome tem ${nome.length} letras <br />`;
+document.body.innerHTML += `A segunda letra do seu nome é: ${nome[1]} <br/>`;
+document.body.innerHTML += `Qual o primeiro índice da letra a no seu nome? : ${nome.indexOf("a")} <br/>`;
 
-window.document.body.innerHTML += "Blablaball";
-
+document.body.innerHTML += `Qual o último índice da letra a no seu nome: ${nome.lastIndexOf("a")} <br/>`;
+document.body.innerHTML += `As últimas 3 letras do seu nome são: ${nome.slice(2, 6)}</br>`;
