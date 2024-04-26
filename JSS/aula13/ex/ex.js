08f0d715273606886ff6482b5cb152405acdd738
Marcelo S. Moreira <120063058+marcelo-moreira-code@users.noreply.github.com>
2024-04-26 12:15:17 -0400

    Update ex.js

--- JSS/aula13/ex/ex.js
+++ JSS/aula13/ex/ex.js
@@ -6,7 +6,7 @@ document.body.innerHTML += `A segunda letra do seu nome é: ${nome[1]} <br/>`;
 document.body.innerHTML += `Qual o primeiro índice da letra a no seu nome? : ${nome.indexOf("a")} <br/>`;
 
 document.body.innerHTML += `Qual o último índice da letra a no seu nome: ${nome.lastIndexOf("a")} <br/>`;
-document.body.innerHTML += `As últimas 3 letras do seu nome são: ${nome.length - 3}</br>`;
+document.body.innerHTML += `As últimas 3 letras do seu nome são: ${nome.slice(-3)}</br>`;
 document.body.innerHTML += `As palavras do seu nome são: ${nome.split(' ')} <br/>`;
 document.body.innerHTML += `Seu nome com letras maiúsculas fica: ${nome.toUpperCase()} <br/>`;
 document.body.innerHTML += `Seu nome com letras minúsculas fica: ${nome.toLowerCase()} <br/>`;
