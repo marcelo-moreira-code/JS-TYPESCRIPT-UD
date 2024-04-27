3a20e005a9b9d00554db9115556790aa4da57228
Marcelo S. Moreira <120063058+marcelo-moreira-code@users.noreply.github.com>
2024-04-27 10:39:21 -0400

    Update numbers.js

--- JSS/aula14/numbers.js
+++ JSS/aula14/numbers.js
@@ -1,3 +1,25 @@
 let num1 = 3.14151563546
 let num2 = 12
 let num3 = 15.23
+
+console.log(num1 + num2);
+
+console.log(num1.toString());
+
+//  number.toString(2) -> exibe a forma binária do número
+//console.log(num2.toString(2));
+
+
+
+// toFixed(2); -> para exibir quantas casas decimais desejar
+//console.log(num1.toFixed(2));
+
+
+// Para testar (true/false) se o número na variável é um interiro ou não 
+//console.log(Number.isInteger(num1));
+
+/* Testar se uma operação dará um erro de NAN pelo motivo de: um n * string
+let temp = num3 * "dia";
+console.log(temp);
+console.log(Number.isNaN(temp)); 
+*/
